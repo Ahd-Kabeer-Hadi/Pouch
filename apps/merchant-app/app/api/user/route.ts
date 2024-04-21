53e7220007392c8b6647932b7f78981860620a5d
Ahd-Kabeer-Hadi <ahd.kabeerpi@gmail.com>
2024-04-21 10:43:27 +0530

    fixes

--- apps/merchant-app/app/api/user/route.ts
+++ apps/merchant-app/app/api/user/route.ts
@@ -1,11 +1,16 @@
 import prisma from "@repo/db/client";
+import { create } from "domain";
 import { NextResponse } from "next/server"
 
 const client = prisma;
 
 export const GET = async () => {
-    await prisma.user.create({
-        data:{
+    await prisma.user.upsert({
+        where :{
+            number: '1212121212' 
+        },
+        update: {},
+        create:{
             email: "asd",
             name: "asdasd",
             number: "1212121212",
